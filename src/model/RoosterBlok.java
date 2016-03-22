864c3cc644d031f14bd8a02adf777b973ef0c10a
Kevin <kevin.deboer@student.hu.nl>
2016-03-22 13:48:30 +0100

    Roosterblok ingevuld

--- src/model/RoosterBlok.java
+++ src/model/RoosterBlok.java
@@ -1,5 +1,56 @@
 package model;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class RoosterBlok {
+	private LocalDate beginDatum;
+	private LocalDate eindDatum;
+	private String periode;
+	private ArrayList<Klas> alleKlassen = new ArrayList<Klas>();
+	private ArrayList<Vak> alleVakken = new ArrayList<Vak>();
 	
+	public LocalDate getBeginDatum()	{
+		return beginDatum;
+	}
+	
+	public LocalDate getEindDatum()	{
+		return eindDatum;
+	}
+	
+	public String getPeriode()	{
+		return periode;
+	}
+	
+	public void setBeginDatum(LocalDate bDat)	{
+		beginDatum = bDat;
+	}
+	
+	public void setEindDatum(LocalDate eDat)	{
+		eindDatum = eDat;
+	}
+	
+	public void setPeriode(String per)	{
+		periode = per;
+	}
+	
+	public Klas zoekKlas(String kC)	{
+		Klas return_klas = null;
+		for(Klas k: alleKlassen)	{
+			if(k.getKlasCode().equals(kC))	{
+				return_klas = k;
+			}
+		}
+		return return_klas;
+	}
+	
+	public Vak zoekVak(String vC)	{
+		Vak return_vak = null;
+		for(Vak v: alleVakken)	{
+			if(v.getVakCode().equals(vC))	{
+				return_vak = v;
+			}
+		}
+		return return_vak;
+	}
 }
