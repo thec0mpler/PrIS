155ee67868f657e1b5a300783a5cec360746503c
Kevin <kevin.deboer@student.hu.nl>
2016-03-24 14:53:24 +0100

    init rooster

--- src/controller/RoosterController.java
+++ src/controller/RoosterController.java
@@ -1,6 +1,12 @@
 package controller;
 
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObject;
+
 import model.PrIS;
+import model.Student;
+import model.Vak;
 import server.Conversation;
 import server.Handler;
 
@@ -18,6 +24,26 @@ public class RoosterController implements Handler {
 	}
 	
 	private void mijnRooster(Conversation conversation)	{
+		System.out.println("RoosterController");
 		
+		JsonObject jsonObjectIn = (JsonObject) conversation.getRequestBodyAsJSON();
+		String gebruikersnaam = jsonObjectIn.getString("username");
+		
+		Student student = informatieSysteem.getStudent(gebruikersnaam);
+		
+		JsonArrayBuilder jab = Json.createArrayBuilder(); 				// En uiteindelijk gaat er een JSON-array met...
+		
+		String klasCode = student.getMijnKlas().getKlasCode();
+		
+		for (int i = 0; i < 3; i++) {
+			jab.add(
+				Json.createObjectBuilder()							// daarin voor elk vak een JSON-object...
+					.add("klascode", klasCode)
+					.add("vakcode", "vakcode (value)")
+					.add("vaknaam", "vakname (value)")
+			);
+		}
+		
+		conversation.sendJSONMessage(jab.build().toString());			// terug naar de Polymer-GUI!
 	}
 }
